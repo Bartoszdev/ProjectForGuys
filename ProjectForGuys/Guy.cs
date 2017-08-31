e3f8b1f8e3426bd6266722219fd435fafa416b67
Bartosz Rozinski <bartosz.rozinski@gmail.com>
2017-08-31 23:11:53 +0200

    GuyClass

--- ProjectForGuys/Guy.cs
+++ ProjectForGuys/Guy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ProjectForGuys
 {
@@ -13,7 +14,30 @@ namespace ProjectForGuys
 
         public int GiveCash(int amount)
         {
+            if (amount <= Cash && amount > 0)
+            {
+                Cash -= amount;
+                return amount;
 
+            }
+            else
+            {
+                MessageBox.Show("Nie mam wystarczającej ilości pieniędzy, aby Ci dać " + amount, Name + " powiedział ...");
+                return 0;
+            
+            }
         }
-    }
-}
+    
+        public int ReceiveCash(int amount)
+        {
+            if (amount > 0)
+            {
+                Cash += amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show(amount + " to nie jest ilość pieniedzy, jaką mogę wziąć ", Name + " powiedzia...");
+                return 0;
+            }
+        }
